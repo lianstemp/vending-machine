6b6ce1082dfc83856d951b0dbc33422264a7a4b2
Farhan Aulianda <70313408+lianstemp@users.noreply.github.com>
2024-03-25 11:16:08 +0700

    Update better print change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 drink = ["Coca Cola", "Orange Juice", "Coffe", "Milk Tea", "Yogurt"]
 drinkPrice = [2,10,5,4,8]
-malaysiaMoney = [1, 5, 10, 20, 50, 100]
+malaysiaMoney = [100, 50, 20, 10, 5, 1]
 
 print("Welcome to our vending machine!")
 print("We have the following items available:")
@@ -21,6 +21,16 @@ if inputMoney < drinkPrice[inputDrink-1]:
     print("Not enough money")
 else:
     print("Here is your", drink[inputDrink-1])
-    print("Here is your change:", inputMoney - drinkPrice[inputDrink-1], "MYR")
+    change = inputMoney - drinkPrice[inputDrink-1]
+    # print("Here is your change:", change, "MYR")
+    
+    # try better way to print change
+    print("Here is your change:")
+    for money in malaysiaMoney:
+        count = change // money
+        if count > 0:
+            print(count, "MYR", money)
+            change %= money
+    
     print("Thank you for using our vending machine!")
 
